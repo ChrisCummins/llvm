227510f83f53b2daecedd0e1bcdb8400ba7057e1
Chris Cummins <chrisc.101@gmail.com>
2014-10-25 23:30:17 +0100

    lib/MemoryCounter: Change test program

--- lib/Transforms/MemoryCounter/test.cpp
+++ lib/Transforms/MemoryCounter/test.cpp
@@ -1,13 +1,29 @@
 #include <cstdio>
 
+int g = 0;
+
 int foo() {
+    // Perform 10 STOREs:
+    g = 1; g = 2; g = 3; g = 4; g = 5;
+    g = 6; g = 7; g = 7; g = 9; g = 10;
+
+    // throw 0;
+
+    // Perform 10 STOREs:
+    g = 1; g = 2; g = 3; g = 4; g = 5;
+    g = 6; g = 7; g = 7; g = 9; g = 10;
+
     return 0;
 }
 
 int main(int argc, char **argv) {
-    std::printf("Hello, world!\n");
+    // 3 STOREs.
 
-    int a = foo();
+    //try {
+        foo();
+    //} catch (...) {}
 
-    return a;
+    // Perform 1 LOAD:
+    std::printf("g = %d\n", g);
+    return 0;
 }
